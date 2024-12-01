be96ca9719cc7c7a2d1ff877b914a9efe4e087bc
duy366110 <duy366110@gmail.com>
2024-12-01 19:28:31 +0700

    #Commit app alias

---
+++ react-admin-tickets/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+      "./src/**/*.{js,jsx,ts,tsx}", // Chỉ định các file để Tailwind có thể áp dụng
+  ],
+  theme: {
+      extend: {},
+  },
+  plugins: [],
+};
--- react-admin-tickets/vite.config.ts
+++ react-admin-tickets/vite.config.ts
@@ -1,19 +1,25 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    define: {
-        'process.env': process.env
-      },
-    server: {
-        host: true,
+  plugins: [react()],
+  define: {
+    "process.env": process.env,
+  },
+  server: {
+    host: true,
+  },
+  base: "./",
+  test: {
+    globals: true,
+    environment: "jsdom",
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
     },
-    base: './',
-    test: {
-        globals: true,
-        environment: 'jsdom'
-    }
+  },
 });
