a66a9c29d463dd57bbb61cbb64c84caf9e689531
duy366110 <duy366110@gmail.com>
2024-12-06 09:19:22 +0700

    Commit ticket

--- react-admin-tickets/src/containers/tickets/Index.tsx
+++ react-admin-tickets/src/containers/tickets/Index.tsx
@@ -1,9 +1,11 @@
 import TicketPageList from "./pages/TicketPageList";
 import TicketPageCrate from "./pages/TicketPageCreate";
 import TicketPageEdit from "./pages/TicketPageEdit";
+import TicketPageDetail from "./pages/TicketPageDetail";
 
 export const resource = {
   list: <TicketPageList />,
   create: <TicketPageCrate />,
   edit: <TicketPageEdit />,
+  show: <TicketPageDetail />
 };
