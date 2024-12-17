aae4f94fa94e9ce5797ee7bd66dfdd8615f23a0c
duy366110 <duy366110@gmail.com>
2024-12-17 10:41:03 +0700

    #Commit setup page edit

--- react-admin-tickets/src/containers/issues/Index.tsx
+++ react-admin-tickets/src/containers/issues/Index.tsx
@@ -1,7 +1,9 @@
 import IssuePageList from "./pages/IssuePageList";
 import IssuePageCreate from "./pages/IssuePageCreate";
+import IssuePageEdit from "./pages/IssuePageEdit";
 
 export const resource = {
     list: IssuePageList,
     create: IssuePageCreate,
+    edit: IssuePageEdit,
 }
